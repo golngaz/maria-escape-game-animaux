f00dd1f49ba0873d028092415efc61085ed4c599
golngaz <golngaz@laposte.net>
2024-06-23 17:12:26 +0200

    fix taill labo img + ortho

--- script/potion-sound-hidden.ts
+++ script/potion-sound-hidden.ts
@@ -6,7 +6,7 @@ export default (scene: SceneToBuild, game: Game) => {
     scene.img(img);
 
     scene.title('Poudlard - Salle de cours de potions magiques - mal caché dans un coin');
-    scene.dialog('Vous vous cachez...\n\n\n\nVous n\'allez pas rester la toute la journée ?');
+    scene.dialog('Vous vous cachez...\n\n\n\nVous n\'allez pas rester là toute la journée ?');
 
     scene.choice('Prendre son courage à deux mains').link('potion-making').onClick(game => game.score++);
 };
