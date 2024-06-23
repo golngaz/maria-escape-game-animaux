744c20834c2772e13083ad136da72546de1e32ae
golngaz <golngaz@laposte.net>
2024-06-23 22:18:25 +0200

    derniers fix

--- script/potion-sound.ts
+++ script/potion-sound.ts
@@ -7,7 +7,7 @@ export default (scene: SceneToBuild, game: Game) => {
     scene.img(img);
 
     scene.title('Poudlard - Salle de cours de potions magiques');
-    scene.dialog('*Vous entendez un bruit*\n\nDans la panique, Nifleur vient de faire tomber la fiole\nde potions "Lumos", qui se brise\n en milles morceaux');
+    scene.dialog('*Vous entendez un bruit*\n\nDans la panique, Nifleur vient de faire tomber la fiole\nde potions "Lumos", qui se brise\n en mille morceaux');
 
     scene.choice('Partir en courant').link('potion-sound-hidden').onClick(game => game.score -= 2);
     scene.choice('Pas le choix... On doit refaire la potion !').link('potion-making')
